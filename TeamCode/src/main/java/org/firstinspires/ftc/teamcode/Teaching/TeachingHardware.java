e509af7f34d207c3b6005a31246796edb779eb85
Dragotamer46 <vivek.sreenivasan3@gmail.com>
2017-03-02 17:07:44 -0500

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teaching/TeachingHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teaching/TeachingHardware.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Teaching;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by drjekyll on 2/28/17.
@@ -10,10 +11,48 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class TeachingHardware {
     public DcMotor left;
     public DcMotor right;
-    public HardwareMap hwmap = null;
-    public void start()
+    public DcMotor shooter;
+    public DcMotor firstSweeper;
+    public DcMotor scndSweeper;
+
+    public Servo beacon;
+
+    private final static double beaconHome = 0.43;
+
+    HardwareMap hwmap = null;
+
+    public TeachingHardware(){
+
+    }
+
+    public void start(HardwareMap ahwmap)
     {
+        hwmap = ahwmap;
         right = hwmap.dcMotor.get("right");
         left = hwmap.dcMotor.get("left");
+        shooter = hwmap.dcMotor.get("shooter");
+        firstSweeper = hwmap.dcMotor.get("firstSweeper");
+        scndSweeper = hwmap.dcMotor.get("scndSweeper");
+
+        beacon = hwmap.servo.get("beacon");
+
+        left.setDirection(DcMotor.Direction.REVERSE);
+
+
+        left.setPower(0);
+        right.setPower(0);
+
+        shooter.setPower(0);
+        firstSweeper.setPower(0);
+        scndSweeper.setPower(0);
+
+        beacon.setPosition(beaconHome);
+
+        left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        shooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        firstSweeper.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        scndSweeper.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teaching/Teaching_TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teaching/Teaching_TeleOP.java
@@ -6,15 +6,48 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
  * Created by drjekyll on 2/28/17.
  */
 
-public class TeachingTeleOP extends LinearOpMode {
+public class Teaching_TeleOP extends LinearOpMode {
     TeachingHardware bob  = new TeachingHardware();
     public void runOpMode() {
+        bob.start(hardwareMap);
         waitForStart();
         while(opModeIsActive()) {
             double left = gamepad1.left_stick_y;
             double right = gamepad1.right_stick_y;
             bob.left.setPower(left);
             bob.right.setPower(right);
+
+            if (gamepad1.a)
+            {
+                bob.firstSweeper.setPower(0.5);
+            }
+            else {
+                bob.firstSweeper.setPower(0);
+            }
+
+            if (gamepad1.x) {
+                bob.scndSweeper.setPower(0.5);
+            }
+            else {
+                bob.scndSweeper.setPower(0);
+            }
+
+            if (gamepad1.y) {
+                bob.shooter.setPower(1.0);
+            }
+            else {
+                bob.shooter.setPower(0);
+            }
+
+            if(gamepad1.right_trigger > 0.25) {
+                double temp = bob.beacon.getPosition();
+                bob.beacon.setPosition(temp - 0.02);
+            }
+            else {
+                double temp = bob.beacon.getPosition();
+                bob.beacon.setPosition(temp + 0.02);
+            }
+
             idle();
         }
     }
